921298fb633e3b77f0165e8456303fb1779195d8
Manuel Bonilla <290857276+tailsbd25-create@users.noreply.github.com>
2026-06-20 13:14:51 +0000

    Add main.h managing standard library and project dependencies

---
+++ main.h
@@ -0,0 +1,18 @@
+/*******************************************************************************
+ * Name: Manuel Bonilla
+ * Assignment: Assignment 4 - Simple Classes & Objects
+ * File: main.h
+ * Date completed: 6/20/2026
+ * Description: This is the library for all the includes used in the program. 
+ * All standard library headers are placed here to keep .cpp files clean.
+ ******************************************************************************/
+ 
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include "stack.h"
+
+#endif
